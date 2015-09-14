a771fe540e201259ca8f9855d32cf90e0c8d6fbb
SkyChen2012 <zjxuch@gmail.com>
2015-09-14 18:58:43 +0800

    添加打印PN  添加 头文件

---
+++ PN_test.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+#define TAG "PN_test"
+#define sucess  1
+#define fail    0
+
+
+#define PN "704-1234-1234-1253-00"
+#define PN_LENGTH 21
+
+int printfPN(char pn[])
+{   
+    printf("PN = %s \n",pn);
+    return sucess;
+}
+
+
+int main()
+{
+    // step 1. 获取 pn 
+    // add code ......
+    
+    printfPN(PN);
+    //step 2. 解析pn  
+    
+    
+    
+    return fail;
+}
